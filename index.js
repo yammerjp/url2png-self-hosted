8f13e7ae0e1289b2edff787474561b0010b26210
Keisuke Nakayama <me@yammer.jp>
2023-03-06 18:54:35 +0900

    wip

--- index.js
+++ index.js
@@ -7,52 +7,19 @@ const path = require('path')
 const IMAGE_CACHE_DIR = path.join(process.cwd(), 'img');
 
 const main = async () => {
-    const cluster = await Cluster.launch({
-        concurrency: Cluster.CONCURRENCY_CONTEXT,
-        maxConcurrency: 10,
-        puppeteerOptions: {
-          headless: true,
-          defaultViewport: {
-              width: 1440,
-              height: 900,
-          },
-          args: [
-            "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36", '--no-sandbox', '--disable-setuid-sandbox'
-          ],
-        }
-    });
-    await cluster.task(async ({ page, data: url}) => {
-        await page.goto(url);
-        await page.waitForTimeout(2000);
-        const screen = await page.screenshot({path: cachePath(url)});
-        return screen;
-    });
 
     app.get('/', async function (req, res) {
       const url = decodeBase64Url(req.query.url_base64url)
-        if (!url) {
-            return res.end('Please specify url like this: ?url=example.com');
-        }
-        try {
           let data = await readFromCache(url)
-          if (!data) {
-            data = await cluster.execute(url);
-          }
 
           res.writeHead(200, {
               'Content-Type': 'image/jpg',
-              'Content-Length': data.length
+              'Content-Length': 0
           });
-          res.end(data);
-
-        } catch (err) {
-            res.end('Error: ' + err.message);
-        }
+          res.end('');
     });
 
-    app.listen(3000, function () {
-        console.log('Screenshot server listening on port 3000.');
-    });
+    app.listen(3000, '0.0.0.0');
 }
 
 const readFromCache = async (url) => {
